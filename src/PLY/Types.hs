a500335aa5023d674209d13c25068d71f1cc5db6
Anthony Cowley <acowley@gmail.com>
2013-05-21 18:04:45 -0600

    Support loading C types.
    Supporting CFloat, etc., as destination types for PLY data gets us
    support for OpenGL.

--- src/PLY/Types.hs
+++ src/PLY/Types.hs
@@ -4,6 +4,7 @@ module PLY.Types (module Data.Int, module Data.Word,
 import Data.ByteString (ByteString)
 import Data.Int
 import Data.Word
+import Foreign.C.Types (CFloat, CDouble, CInt, CUChar)
 import Foreign.Storable (Storable)
 
 data Format = ASCII | Binary_LE | Binary_BE deriving Show
@@ -38,17 +39,37 @@ instance PLYType Float where
   unsafeUnwrap (Sfloat x) = x
   unsafeUnwrap y = error $ "Tried to unwrap "++show y++" as a Float"
 
+instance PLYType CFloat where
+  plyType _ = Tfloat
+  unsafeUnwrap (Sfloat x) = realToFrac x
+  unsafeUnwrap y = error $ "Tried to unwrap "++show y++" as a CFloat"
+
 instance PLYType Double where
   plyType _ = Tdouble
   unsafeUnwrap (Sdouble x) = x
   unsafeUnwrap y = error $ "Tried to unwrap "++show y++" as a Double"
+
+instance PLYType CDouble where
+  plyType _ = Tdouble
+  unsafeUnwrap (Sdouble x) = realToFrac x
+  unsafeUnwrap y = error $ "Tried to unwrap "++show y++" as a CDouble"
 
 instance PLYType Int where
   plyType _ = Tint
   unsafeUnwrap (Sint x) = x
   unsafeUnwrap y = error $ "Tried to unwrap "++show y++" as an Int"
 
+instance PLYType CInt where
+  plyType _ = Tint
+  unsafeUnwrap (Sint x) = fromIntegral x
+  unsafeUnwrap y = error $ "Tried to unwrap "++show y++" as an CInt"
+
 instance PLYType Word8 where
   plyType _ = Tuchar
   unsafeUnwrap (Suchar x) = x
   unsafeUnwrap y = error $ "Tried to unwrap "++show y++" as a Word8"
+
+instance PLYType CUChar where
+  plyType _ = Tuchar
+  unsafeUnwrap (Suchar x) = fromIntegral x
+  unsafeUnwrap y = error $ "Tried to unwrap "++show y++" as a CUChar"
