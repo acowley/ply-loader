1819104fe00e2cbccc79736c7758fe98e7290e08
Anthony Cowley <acowley@gmail.com>
2016-10-23 03:54:07 -0400

    Update to vector 0.11 API

--- src/PLY/Internal/StrictReplicate.hs
+++ src/PLY/Internal/StrictReplicate.hs
@@ -1,19 +1,16 @@
 {-# LANGUAGE BangPatterns #-}
 module PLY.Internal.StrictReplicate where
-import Data.Vector.Generic (Vector, unstream)
-import Data.Vector.Fusion.Stream (unsafeFromList)
-import Data.Vector.Fusion.Stream.Monadic (Stream (..), Step(..), size, toList)
-import Data.Vector.Fusion.Util (delay_inline)
-import Data.Vector.Fusion.Stream.Size (Size(..))
+import Data.Vector.Generic (Vector, fromList)
+import Data.Vector.Fusion.Bundle.Monadic (fromStream, toList)
+import Data.Vector.Fusion.Bundle.Size (Size(..))
+import Data.Vector.Fusion.Stream.Monadic (Stream (..), Step(..))
 
 -- | Yield a 'Stream' of values obtained by performing the monadic
 -- action the given number of times. Each value yielded by the monadic
 -- action is evaluated to WHNF.
 replicateStreamM' :: Monad m => Int -> m a -> Stream m a
 {-# INLINE [1] replicateStreamM' #-}
--- NOTE: We delay inlining max here because GHC will create a join point for
--- the call to newArray# otherwise which is not really nice.
-replicateStreamM' n p = Stream step n (Exact (delay_inline max n 0))
+replicateStreamM' n p = Stream step n
   where
     {-# INLINE [0] step #-}
     step i | i <= 0    = return Done
@@ -24,6 +21,6 @@ replicateStreamM' n p = Stream step n (Exact (delay_inline max n 0))
 -- evaluated to WHNF.
 replicateM' :: (Monad m, Vector v a) => Int -> m a -> m (v a)
 replicateM' n p = do let s = replicateStreamM' n p
-                     xs <- toList s
-                     return . unstream $ unsafeFromList (size s) xs
+                     xs <- toList (fromStream s (Exact n))
+                     return (fromList xs)
 {-# INLINE replicateM' #-}
